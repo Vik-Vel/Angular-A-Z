6d2bde69609bb1acdb7a6a39ba33f6bb539c0c8c
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-08-22 08:10:30 +0300

    feat: implement form for adding new wishes and update component structure

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,24 +1,36 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishItem';
 import { NgFor, NgIf, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf, NgClass],
+  imports: [NgFor, NgIf, NgClass, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  items : WishItem[] = [
+  items: WishItem[] = [
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
+    new WishItem('Find grass that cuts itself'),
   ];
+
+  newWishText = '';
+
   title = 'wishlist';
 
-  toggleItem(item : WishItem){
-    item.isCompleted = !item.isCompleted
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+
+    //todo - add wish to items array
+    //clear the textbox
+  }
+
+  toggleItem(item: WishItem) {
+    item.isCompleted = !item.isCompleted;
     console.log(item);
   }
 }
